a2074548788d7e156c836f197d33369a34b0ee05
Miroslav DImitrov <miro_sv@yahoo.com>
2024-01-11 17:25:35 +0200

    make form controlled

--- ze-winery-app/src/components/Products/ProductQuantityForm/ProductQuantityForm.jsx
+++ ze-winery-app/src/components/Products/ProductQuantityForm/ProductQuantityForm.jsx
@@ -2,13 +2,28 @@
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
 
 
 
 
 const ProductQuantityForm = ({showModal,closeModal ,currentItem}) => {
 
-  console.log(currentItem);
+ const [itemQuantity, setItemQuantity] = useState('')
+
+
+ const itemQuantityHandler = (e) => {
+  setItemQuantity(e.target.value);
+  
+ }
+
+ const onSubmitQuantityValue = (e) => {
+ e.preventDefault();
+ closeModal()
+ console.log(itemQuantity);
+ console.log(currentItem);
+
+ }
 
     return (
       <>
@@ -17,22 +32,24 @@ const ProductQuantityForm = ({showModal,closeModal ,currentItem}) => {
         <Modal.Title>{currentItem.trademarkName} {currentItem.pieces}</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Form>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+      
+         
             <Form.Label>Количество</Form.Label>
             <Form.Control
               type="text"
               autoFocus
+              value={itemQuantity}
+              onChange={itemQuantityHandler}
             />
-          </Form.Group>
         
-        </Form>
+        
+   
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={closeModal}>
           Затвори
         </Button>
-        <Button variant="primary" onClick={closeModal}>
+        <Button variant="primary" onClick={onSubmitQuantityValue}>
           Добави
         </Button>
       </Modal.Footer>
