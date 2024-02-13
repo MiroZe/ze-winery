773bcd841bb1cdc2f83d4b3846f4c001d9b72c77
Miroslav DImitrov <miro_sv@yahoo.com>
2024-02-13 21:27:52 +0200

    Update SubbmittedDeclarations.jsx

--- ze-winery-app/src/components/Declarations/SubmittedDeclarations/SubbmittedDeclarations.jsx
+++ ze-winery-app/src/components/Declarations/SubmittedDeclarations/SubbmittedDeclarations.jsx
@@ -1,6 +1,5 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import style from './SubbmittedDeclarations.module.css'
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
