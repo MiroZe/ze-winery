698835e9a866c569f992af9fd06eabb6cec5d7dd
Miroslav DImitrov <miro_sv@yahoo.com>
2024-02-28 11:44:05 +0200

    format date at mongoose declaration model

--- ze-winery-server/models/declarationModel.js
+++ ze-winery-server/models/declarationModel.js
@@ -47,7 +47,16 @@ const declarationSchema = new mongoose.Schema({
   },
   preparationDate: {
     type:String,
-    default: Date.now,
+    default: function() {
+     
+      const currentDate = new Date();
+
+      const day = String(currentDate.getDate()).padStart(2, '0');
+      const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+      const year = currentDate.getFullYear();
+
+      return `${day}${month}${year}`;
+  },
     required: true
   },
   customsOffice: {
