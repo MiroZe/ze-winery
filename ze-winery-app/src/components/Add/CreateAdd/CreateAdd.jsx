06a349c4c9503e490e8874568e723729f268ee3b
Miroslav DImitrov <miro_sv@yahoo.com>
2024-03-02 18:27:01 +0200

    some progres

--- ze-winery-app/src/components/Add/CreateAdd/CreateAdd.jsx
+++ ze-winery-app/src/components/Add/CreateAdd/CreateAdd.jsx
@@ -135,7 +135,25 @@ const CreateAdd = () => {
 
             <div className={styles['bottom-part']}>
                 <Row>
-                {definedAdData.map((r,index) => <Col Col key={index}><span>{r} </span></Col> )}
+                    <Col className={styles['vertical-col']}><span>No:</span></Col>
+                    <Col className={styles['vertical-col']}><span>Търговско наименование</span></Col>
+                    <Col className={styles['vertical-col']}><span>Търговска марка</span></Col>
+                    <Col className={styles['vertical-col']}><span>АКР No: на е-ЕАД или No: на друг документ</span></Col>
+                    <Col className={styles['vertical-col']}><span>Действителни количества</span></Col>
+                    <Col className={styles['vertical-col']}><span>УИКТ-уникален идентиФикатор на контролна точка</span></Col>
+                    <Col className={styles['vertical-col']}><span>No: на транзакцията</span></Col>
+                    <Col className={styles['vertical-col']}><span>Стойност на сделката</span></Col>
+                    <Col className={styles['vertical-col']}><span>код на АП</span></Col>
+                    <Col className={styles['vertical-col']}><span>Код по КН</span></Col>
+                    <Col className={styles['vertical-col']}><span>Допълнитепен код на стоката</span></Col>
+                    <Col className={styles['vertical-col']}><span>Основание за възникване на задължение эа начисляване на аициз или недължимост на акциза(предназначение)</span></Col>
+                    <Col className={styles['vertical-col']} ><span>Апкохолно съдържание/ градус плато /дължина на цигарата без Филтьра или мундщука/горна топлотворна способност</span></Col>
+                    <Col className={styles['vertical-col']} ><span>Продажна цена</span></Col>
+                    <Col className={styles['vertical-col']} ><span>Вместимост на потребителската опаковка</span></Col>
+                    <Col className={styles['vertical-col']} ><span>брой потребителски опаковки</span></Col>
+                    <Col className={styles['vertical-col']} ><span>Мерна единица</span></Col>
+                    <Col className={styles['vertical-col']} ><span>коефициент на преизчиспяване към данъчна основа</span></Col>
+
                 </Row>
             </div>
         </>
