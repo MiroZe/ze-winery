366541bc42094387ac208ebc8f7dda65577ddafa
Miroslav DImitrov <miro_sv@yahoo.com>
2024-03-06 16:09:29 +0200

    create client form

---
+++ ze-winery-app/src/components/Add/CreateAdd/ClientForm.jsx
@@ -0,0 +1,37 @@
+
+
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import styles from './CreateAdd.module.css'
+const ClientForm = () => {
+
+return (
+    <div className={styles['client-container']}>
+    <h3>Получател</h3>
+    <Form>
+        <Form.Group className="mb-3" >
+            <FloatingLabel label="Име">
+                <Form.Control type="text" />
+            </FloatingLabel>
+            <FloatingLabel label='ЕИК'>
+                <Form.Control type="text" />
+            </FloatingLabel>
+            <FloatingLabel label='Адрес'>
+                <Form.Control type="text" />
+            </FloatingLabel>
+            <FloatingLabel label='Град'>
+                <Form.Control type="text" />
+            </FloatingLabel>
+
+        </Form.Group>
+    </Form>
+
+
+</div>
+
+)
+
+
+}
+
+export default ClientForm;
--- ze-winery-app/src/components/Add/CreateAdd/CreateAdd.jsx
+++ ze-winery-app/src/components/Add/CreateAdd/CreateAdd.jsx
@@ -2,42 +2,45 @@ import { useSelector } from 'react-redux';
 import styles from './CreateAdd.module.css';
 import Form from 'react-bootstrap/Form';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import ClientForm from './ClientForm';
 
 const CreateAdd = () => {
 
     const company = useSelector(state => state.company);
-    console.log(company);
-
-
-return (
-<div className={styles['add-form-container']}>
-
-    <div className={styles['company-container']}>
-        <h3>Издател</h3>
-        <Form>
-      <Form.Group className="mb-3" >
-        <FloatingLabel label="Име">
-        <Form.Control type="text"  disabled defaultValue={`${company.companyName} ${company.companyType}`}/>
-        </FloatingLabel>
-        <FloatingLabel label='ЕИК'>
-        <Form.Control type="text"  disabled defaultValue={company.companyId}/>
-        </FloatingLabel>
-        <FloatingLabel label='Адрес'>
-        <Form.Control type="text"  disabled defaultValue={company.address}/>
-        </FloatingLabel>
-        <FloatingLabel label='Град'>
-        <Form.Control type="text"  disabled defaultValue={company.city}/>
-        </FloatingLabel>
-      </Form.Group>
-
  
-    </Form>
 
-    </div>
 
-</div>
+    return (
+        <div className={styles['add-form-container']}>
+            <ClientForm/>
 
-)
+          
+            <h3>Номер</h3>
+            <div className={styles['company-container']}>
+                <h3>Издател</h3>
+                <Form>
+                    <Form.Group className="mb-3" >
+                        <FloatingLabel label="Име">
+                            <Form.Control type="text" disabled defaultValue={`${company.companyName} ${company.companyType}`} />
+                        </FloatingLabel>
+                        <FloatingLabel label='ЕИК'>
+                            <Form.Control type="text" disabled defaultValue={company.companyId} />
+                        </FloatingLabel>
+                        <FloatingLabel label='Адрес'>
+                            <Form.Control type="text" disabled defaultValue={company.address} />
+                        </FloatingLabel>
+                        <FloatingLabel label='Град'>
+                            <Form.Control type="text" disabled defaultValue={company.city} />
+                        </FloatingLabel>
+                    </Form.Group>
+
+
+                </Form>
+            </div>
+
+        </div>
+
+    )
 
 
 };
