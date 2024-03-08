a2b3a62052bc1d9b6b26b39aa52dc38639a691ff
Miroslav DImitrov <miro_sv@yahoo.com>
2024-03-08 16:51:01 +0200

    create form client with search filed

--- ze-winery-app/src/components/Add/CreateAdd/ClientForm.jsx
+++ ze-winery-app/src/components/Add/CreateAdd/ClientForm.jsx
@@ -1,7 +1,10 @@
 
 import Form from 'react-bootstrap/Form';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import styles from './CreateAdd.module.css'
+import styles from './CreateAdd.module.css';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 
 
@@ -9,8 +12,24 @@ const ClientForm = () => {
 
 return (
     <div className={styles['client-container']}>
+        <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+        <Form.Label column sm="2">
+          Търси клиент
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control plaintext   />
+        </Col>
+        <Form.Select aria-label="Default select example">
+      <option>Open this select menu</option>
+      <option value="1">One</option>
+      <option value="2">Two</option>
+      <option value="3">Three</option>
+    </Form.Select>
+      </Form.Group>
     <h3>Получател</h3>
+
     <Form>
+    
         <Form.Group className="mb-3" >
             <FloatingLabel label="Име">
                 <Form.Control type="text" />
@@ -26,6 +45,7 @@ return (
             </FloatingLabel>
 
         </Form.Group>
+        <Button variant="outline-success">Запиши</Button>
     </Form>
 
 
--- ze-winery-app/src/components/Add/CreateAdd/CreateAdd.jsx
+++ ze-winery-app/src/components/Add/CreateAdd/CreateAdd.jsx
@@ -34,7 +34,7 @@ const CreateAdd = () => {
                         </FloatingLabel>
                     </Form.Group>
 
-
+                    
                 </Form>
             </div>
 
