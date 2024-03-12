0ba66d46cfdbcf85072acaa565c4d53bd074956e
Miroslav DImitrov <miro_sv@yahoo.com>
2024-03-12 19:30:43 +0200

    Update ClientForm.jsx

--- ze-winery-app/src/components/Add/CreateAdd/ClientForm.jsx
+++ ze-winery-app/src/components/Add/CreateAdd/ClientForm.jsx
@@ -13,49 +13,61 @@ import { useState } from 'react';
 
 const ClientForm = () => {
 
-    const {formValues,onChangeHandler} = useForm();
+    const { formValues, onChangeHandler } = useForm();
     const [showClientForm, setShowClientForm] = useState(false)
 
     return (
         <div className={styles['client-container']}>
             <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
                 <Form.Label column sm="2">
-                    Търси 
+                    Търси
                 </Form.Label>
                 <Col sm="10">
-                    <Form.Control type='text' defaultValue='11111'  />
+                    <Form.Control type='text' defaultValue='11111' />
 
                 </Col>
             </Form.Group>
             <ListGroup as="ul">
-      <ListGroup.Item as="li" active>
-        Cras justo odio
-      </ListGroup.Item>
-      <ListGroup.Item as="li">Dapibus ac facilisis in</ListGroup.Item>
-      <ListGroup.Item as="li" disabled>
-        Morbi leo risus
-      </ListGroup.Item>
-      <ListGroup.Item as="li">Porta ac consectetur ac</ListGroup.Item>
-    </ListGroup>
+                <ListGroup.Item as="li" active>
+                    Cras justo odio
+                </ListGroup.Item>
+                <ListGroup.Item as="li">Dapibus ac facilisis in</ListGroup.Item>
+                <ListGroup.Item as="li" disabled>
+                    Morbi leo risus
+                </ListGroup.Item>
+                <ListGroup.Item as="li">Porta ac consectetur ac</ListGroup.Item>
+            </ListGroup>
             <h3>Получател</h3>
 
             <Form>
+                <Row className="mb-3">
+                    <Form.Group>
+                        <FloatingLabel label="Име">
+                            <Form.Control type="text" />
+                        </FloatingLabel>
+                    </Form.Group>
 
-                <Form.Group className="mb-3" >
-                    <FloatingLabel label="Име">
-                        <Form.Control type="text" />
-                    </FloatingLabel>
-                    <FloatingLabel label='ЕИК'>
-                        <Form.Control type="text" />
-                    </FloatingLabel>
-                    <FloatingLabel label='Адрес'>
-                        <Form.Control type="text" />
-                    </FloatingLabel>
-                    <FloatingLabel label='Град'>
-                        <Form.Control type="text" />
-                    </FloatingLabel>
+                    <Form.Select required name='companyType' >
+                        <option value="">Изберете...</option>
+                        <option value="ЕТ">ЕТ</option>
+                        <option value="ЕООД">ЕООД</option>
+                        <option value="ООД">ООД</option>
+                        <option value="ЕАД">ЕАД</option>
+                        <option value="АД">АД</option>
+                        <option value="СД">СД</option>
+                    </Form.Select>
+                </Row>
+                <FloatingLabel label='ЕИК'>
+                    <Form.Control type="text" />
+                </FloatingLabel>
+                <FloatingLabel label='Адрес'>
+                    <Form.Control type="text" />
+                </FloatingLabel>
+                <FloatingLabel label='Град'>
+                    <Form.Control type="text" />
+                </FloatingLabel>
 
-                </Form.Group>
+
                 <Button variant="outline-success">Запиши</Button>
             </Form>
 
