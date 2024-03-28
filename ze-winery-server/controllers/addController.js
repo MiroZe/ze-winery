54a72d685c50640c1bddb4626e381a8023b24857
Miroslav DImitrov <miro_sv@yahoo.com>
2024-03-28 11:22:00 +0200

    Update addController.js

--- ze-winery-server/controllers/addController.js
+++ ze-winery-server/controllers/addController.js
@@ -1,9 +1,24 @@
+const { clientCompanyModel, clientModel } = require("../models");
 
 
 
-const clientCreate = (req,res,next) => {
+const clientCreate = async (req,res,next) => {
 
-console.log(req.body);
+const{name,address,city,companyType,companyId,isCompany,ownerId} = req.body;
+let createdClient;
+
+try {
+    if(isCompany) {
+     createdClient = await clientCompanyModel.create({name,address,city,companyType,companyId,ownerId});
+    } else {
+        createdClient = await clientModel.create({name,address,city,ownerId});
+    }
+    
+    return res.status(201).json(createdClient) ;
+} catch (error) {
+    next(error)
+}
+
 
 }
 
