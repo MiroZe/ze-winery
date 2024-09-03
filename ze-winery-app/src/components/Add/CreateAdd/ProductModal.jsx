8af72acb21a08b76b5e04be5ebe0fdf1c55a9c14
Miroslav DImitrov <miro_sv@yahoo.com>
2024-09-03 13:29:17 +0300

    Update ProductModal.jsx

--- ze-winery-app/src/components/Add/CreateAdd/ProductModal.jsx
+++ ze-winery-app/src/components/Add/CreateAdd/ProductModal.jsx
@@ -1,6 +1,5 @@
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
-import ProductsList from '../../Products/ProductsList';
 
 const ProductModal = ({showProductModal, setShowProductModal}) => {
  
